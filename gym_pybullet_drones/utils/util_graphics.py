bb8b86772294222df693f4b81f5edd8a9b104103
kyrran <kangle.yuan@outlook.com>
2024-10-23 16:16:50 +0800

    Display the result only

--- gym_pybullet_drones/utils/util_graphics.py
+++
@@ -1,9 +0,0 @@
-"""https://github.com/TommyWoodley/TommyWoodleyMEngProject"""
-
-
-def print_red(text):
-    print(f"\033[31m{text}\033[0m")
-
-
-def print_green(text):
-    print(f"\033[32m{text}\033[0m")
